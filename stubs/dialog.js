573e54d650599f503ff2bea16bc9e327e14a254c
ramunune <rnune@connexcs.com>
2022-10-31 12:23:06 +0000

    Add Methods For Dialog.

--- stubs/dialog.js
+++ stubs/dialog.js
@@ -30,17 +30,39 @@ function dialog() {
 	function close () {
 	}
 	/**
-		* Get A Component
-		* @function getComponent
+		* Validate The Form In Dialog
+		* @function validate
 		* @memberof dialog
-		* @param {string} name - The component name
-		* @returns {Object} - Component Object
 		* @example
-		* this.getComponent('dialog')
-		* @description 'dialog' is the ID field in the 'Component Attribute' tab
+		* var myDialog = this.getComponent('myDialog')
+		* myDialog.validate()
 	*/
-	function getComponent (name) {
+	function validate () {
 	}
+	/**
+		* Validate The Form In Dialog
+		* @function setOptions
+		* @memberof dialog
+		* @example
+		* var myDialog = this.getComponent('myDialog')
+		* var fields = []
+		* var options = []
+		* myDialog.setOptions(fields, options)
+	*/
+	function setOptions (fields, options) {
+	}
+	/**
+	* Set Data
+	* @function setData
+	* @memberof page
+	* @param {Object} Value - Key-Value pair
+	* @apiDescription setData can be used to populate a page/form with information stored in a key:value object
+	* @example
+	* var myData = {name: 'Joe Blogs', age: 30};
+	* this.setData(myData);
+	 */
+	function setData (value) {
+	},
 	/**
 		* Get Form Values From A Component
 		* @function getValues()
@@ -49,7 +71,8 @@ function dialog() {
 		* @example
 		* var myDialog = this.getComponent('myDialog')
 		* myDialog.getValues()
-		* @description To get form data values from a component
+		* @description Get the values of all fields in the dialg
+		* @returns {name: 'XYZ', age: 28, country: 'United Kingdom'}
 	*/
 	function getValues () {
 	}
@@ -57,13 +80,59 @@ function dialog() {
 		* Get A Value From An Object
 		* @function getValue
 		* @memberof dialog
-		* @params {string} fieldName - Field Name
+		* @param {string} fieldName - Field Name
 		* returns Value of the selected field
 		* @example
 		* var myData = {name: 'Joe Blogs', age: 30};
-		* this.getValue('name')
+		* var myDialog = this.getComponent('myDialog')
+		* myDialog.getValue('name')
 		* @returns 'Joe Blogs'
 	*/
 	function getValue (fieldName) {
+	}
+	/**
+		* Clear Form Validation In Dialog
+		* @function clearValidate
+		* @memberof dialog
+		* @example
+		* var myDialog = this.getComponent('myDialog')
+		* myDialog.clearValidate()
+	*/
+	function clearValidate () {
+	}
+	/**
+		* Display Fields In The Dialog
+		* @function display
+		* @memberof dialog
+		* @param {Array} fields - Array of items
+		* @example
+		* var myDialog = this.getComponent('myDialog')
+		* var hideFieldsArray = ['name']
+		* myDialog.display(hideFieldsArray)
+	*/
+	function display (fields) {
+	}
+	/**
+		* Hide Fields In The Dialog
+		* @function hide
+		* @memberof dialog
+		* @param [Array] fields - Array of items
+		* @example
+		* var myDialog = this.getComponent('myDialog')
+		* var hideFieldsArray = ['name']
+		* myDialog.hide(hideFieldsArray)
+	*/
+	function hide (fields) {
+	}
+	/**
+		* Set A Spinner On Save Button
+		* @function confirmLoading
+		* @memberof dialog
+		* @param [boolean] [status = false]
+		* @example
+		* var myDialog = this.getComponent('myDialog')
+		* myDialog.confirmLoading(true)
+	*/
+	function confirmLoading (status) {
 	}
 }
