1051cf919dc54bc65854f36bb6606a529dfdd4d9
ramunune <rnune@connexcs.com>
2022-10-31 09:41:31 +0000

    Create Dialog Stub File.

---
+++ stubs/dialog.js
@@ -0,0 +1,51 @@
+/**
+ * ConnexCS Dialog
+ * 
+ * @module Dialog
+ * @namespace dialog
+ * @class
+ * @hideconstructor
+ * @classdesc ConnexCS Dialog Component
+ */
+
+function dialog() {
+	/**
+		* Get A Component
+		* @function getComponent
+		* @memberof dialog
+		* @param {string} name - The component name
+		* @returns {Object} - Component Object
+		* @example
+		* this.getComponent('dialog')
+		* @description 'dialog' is the ID field in the 'Component Attribute' tab
+	*/
+	function getComponent (name) {
+	}
+	/**
+		* Get Form Values From A Component
+		* @function getValues()
+		* @memberof dialog
+		* @returns The Data
+		* @example
+		* this.getComponent('dialog').getValues()
+		* @example
+		* var dialog = this.getComponent('dialog')
+		* dialog.getValues()
+		* @description To get form data values from a component
+	*/
+	function getValues () {
+	}
+	/**
+		* Get A Value From An Object
+		* @function getValue
+		* @memberof dialog
+		* @params {string} fieldName - Field Name
+		* returns Value of the selected field
+		* @example
+		* var myData = {name: 'Joe Blogs', age: 30};
+		* this.getValue('name')
+		* @returns 'Joe Blogs'
+	*/
+	function getValue (fieldName) {
+	}
+}
