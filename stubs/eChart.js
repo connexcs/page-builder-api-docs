87ded9d07c2c4c41d839b55d7a375b20da702000
ramunune <rnune@connexcs.com>
2022-11-01 10:03:07 +0000

    Tidy Page And eChart Files. Add Links In eChart.js.

--- stubs/eChart.js
+++ stubs/eChart.js
@@ -1,72 +1,73 @@
 /**
- * ConnexCS Page
- * 
- * @module eChart
- * @namespace eChart
- * @class
- * @hideconstructor
- * @classdesc ConnexCS eChart Graph Component
- */
+* ConnexCS Page
+* 
+* @module eChart
+* @namespace eChart
+* @class
+* @hideconstructor
+* @classdesc ConnexCS eChart Graph Component
+*/
 
 function eChart() {
 	/**
-		* Set Graph Option
-		* @function setOption
-		* @memberof eChart
-		* @params {Object} option
-		* @params {Boolean} notMerge - Optional. Default is false.
-		* @params {Boolean} lazyUpdate - Opional. Default is false.
-		* @return Returns options object
-		* @description All parameters and data can be modified through setOption
-		* notMerge - Whether not to merge with previous option. If true, all of the current components will be removed and new components will be created according to the new option.
-		* lazyUpdate - Whether not to update chart immediately, stating update chart synchronously. If true, the chart will be updated in the next animation frame.
-		* var option = {
-		*		xAxis: [
-		*			{ id: 'm', interval: 5 },
-		*			{ id: 'n', name: 'nnn', interval: 6 }
-		*			{ id: 'q', interval: 7 }
-		*		]
-		*	}
-		* @example this.setOption(option, notMerge, lazyUpdate)
+	* Set Graph Option
+	* @function setOption
+	* @memberof eChart
+	* @params {Object} option
+	* @params {Boolean} notMerge - Optional. Default is false.
+	* @params {Boolean} lazyUpdate - Opional. Default is false.
+	* @return Returns options object
+	* @description All parameters and data can be modified through setOption
+	* notMerge - Whether not to merge with previous option. If true, all of the current components will be removed and new components will be created according to the new option.
+	* lazyUpdate - Whether not to update chart immediately, stating update chart synchronously. If true, the chart will be updated in the next animation frame.
+	* var option = {
+	*		xAxis: [
+	*			{ id: 'm', interval: 5 },
+	*			{ id: 'n', name: 'nnn', interval: 6 }
+	*			{ id: 'q', interval: 7 }
+	*		]
+	*	}
+	* @example this.setOption(option, notMerge, lazyUpdate)
 	*/
 	function setOption (option = {}, notMerge = false, lazyUpdate = false) {
 	}
 	/**
-		* Get Graph Option Object
-		* @function getOption
-		* @memberof eChart
-		* @return Returns options object
-		* @description Contains configuration item and data merged from previous setOption.
-		* @example this.getOption()
+	* Get Graph Option Object
+	* @function getOption
+	* @memberof eChart
+	* @return Returns options object
+	* @description Contains configuration item and data merged from previous setOption.
+	* @example this.getOption()
 	*/
 	function getOption () {
 	}
 	/**
-		* Resize Graph
-		* @function resize
-		* @memberof eChart
-		* @description Resizes chart, which should be called manually when container size changes.
-		* @example this.resize()
+	* Resize Graph
+	* @function resize
+	* @memberof eChart
+	* @description Resizes chart, which should be called manually when container size changes.
+	* @example this.resize()
 	*/
 	function resize () {
 	}
 	/**
-		* Show Loading Of Graph
-		* @function showLoading
-		* @memberof eChart
-		* @description Shows loading animation effect.
-		* @example this.showLoading()
+	* Show Loading Of Graph
+	* @function showLoading
+	* @memberof eChart
+	* @description Shows loading animation effect.
+	* @description This is opposite of {@link https://page-builder-api-docs.connexcs.com/echart#.hideLoading hideLoading function}
+	* @example this.showLoading()
 	*/
 	function showLoading () {
 	}
 	/**
-		* Hide Loading Of Graph
-		* @function hideLoading
-		* @memberof eChart
-		* @description Hides loading animation effect.
-		* @example this.hideLoading()
+	* Hide Loading Of Graph
+	* @function hideLoading
+	* @memberof eChart
+	* @description Hides loading animation effect.
+	* @description This is opposite of {@link https://page-builder-api-docs.connexcs.com/echart#.showLoading showLoading function}
+	* @example this.hideLoading()
 	*/
 	function hideLoading () {
 	}
 }
-	
--- stubs/page.js
+++ stubs/page.js
@@ -10,89 +10,89 @@
 
 function page() {
 	/**
-		* Refresh
-		* @function refresh
-		* @memberof page
-		* @description refreshs the data
-		* @example
-		* this.refresh()
+	* Refresh
+	* @function refresh
+	* @memberof page
+	* @description refreshs the data
+	* @example
+	* this.refresh()
 	*/
 	function refresh () {
 	}
 	/**
-	 * Set Data
-	 * @function setData
-	 * @memberof page
-	 * @param {Object} Value
-	 * @apiDescription setData can be used to populate a page/form with information stored in a key:value object
-	 * @example
-	 * var myData = {name: 'Joe Blogs', age: 30};
-	 * this.setData(myData);
-	 */
+	* Set Data
+	* @function setData
+	* @memberof page
+	* @param {Object} Value
+	* @apiDescription setData can be used to populate a page/form with information stored in a key:value object
+	* @example
+	* var myData = {name: 'Joe Blogs', age: 30};
+	* this.setData(myData);
+	*/
 	function setData (value) {
 	}
 	/**
-		* Get A Component
-		* @function getComponent
-		* @memberof page
-		* @param {string} name - The component name
-		* @returns {Object} - Component Object
-		* @example
-		* this.getComponent('dialog')
-		* @description 'dialog' is the ID field in the 'Component Attribute' tab
+	* Get A Component
+	* @function getComponent
+	* @memberof page
+	* @param {string} name - The component name
+	* @returns {Object} - Component Object
+	* @example
+	* this.getComponent('dialog')
+	* @description 'dialog' is the ID field in the 'Component Attribute' tab
 	*/
 	function getComponent (name) {
 	}
 	/**
-		* Get Form Values From A Component
-		* @function getValues()
-		* @memberof page
-		* @returns The Data
-		* @example
-		* this.getComponent('dialog').getValues()
-		* @example
-		* var dialog = this.getComponent('dialog')
-		* dialog.getValues()
-		* @description To get form data values from a component
+	* Get Form Values From A Component
+	* @function getValues
+	* @memberof page
+	* @returns The Data
+	* @example
+	* this.getComponent('dialog').getValues()
+	* @example
+	* var dialog = this.getComponent('dialog')
+	* dialog.getValues()
+	* @description To get form data values from a component
 	*/
 	function getValues () {
 	}
 	/**
-		* Get A Value From An Object
-		* @function getValue
-		* @memberof page
-		* @params {string} fieldName - Field Name
-		* returns Value of the selected field
-		* @example
-		* var myData = {name: 'Joe Blogs', age: 30};
-		* this.getValue('name')
-		* @returns 'Joe Blogs'
+	* Get A Value From An Object
+	* @function getValue
+	* @memberof page
+	* @params {string} fieldName - Field Name
+	* returns Value of the selected field
+	* @example
+	* var myData = {name: 'Joe Blogs', age: 30};
+	* this.getValue('name')
+	* @returns 'Joe Blogs'
 	*/
 	function getValue (fieldName) {
 	}
 	/**
-		* Set Rules For A Field
-		* @function setRules
-		* @memberof page
-		* @params {string} field
-		* @params {Array} rules
-		* @example
-		* var field = 'name'
-		* var rules = [
-		* { required: true, message: 'Name is requried' }
-		* ]
-		* this.setRules(field, rules)
+	* Set Rules For A Field
+	* @function setRules
+	* @memberof page
+	* @params {string} field
+	* @params {Array} rules
+	* @example
+	* var field = 'name'
+	* var rules = [
+	* { required: true, message: 'Name is requried' }
+	* ]
+	* this.setRules(field, rules)
 	*/
 	function setRules (field, rules) {
 	}
 	/**
-		* Delete Component
-		* @function deleteComponentInstance
-		* @memberof page
-		* @params {string} key
-		* @example - Delete a Dialog Component
-		* this.deleteComponentInstance('dialog')
-		* @description Deletes the component if it exists
+	* Delete Component
+	* @function deleteComponentInstance
+	* @memberof page
+	* @params {string} key
+	* @example - Delete a Dialog Component
+	* this.deleteComponentInstance('dialog')
+	* @description Deletes the component if it exists
 	*/
 	function deleteComponentInstance (key) {
 	}
